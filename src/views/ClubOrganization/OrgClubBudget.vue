31e8735c821389113d177ec6f0cb69ed127d91ad
qr Fu <f_13@qq.com>
2023-04-25 11:29:27 +0800

    altered budget form

--- src/views/ClubOrganization/OrgClubBudget.vue
+++ src/views/ClubOrganization/OrgClubBudget.vue
@@ -7,63 +7,58 @@
 
             <!-- 财务管理 -->
             <el-card class="box-card-1" style="text-align:left">
-                <el-input style="width: 200px;" v-model="apply.title" />
-                <el-table :data="tableData" style="width: 100%" max-height="250">
-                    <!-- <el-table-column fixed prop="date" label="Date" width="150" /> -->
-                    <el-table-column prop="name" label="Name" width="220" />
-                    <!-- <el-table-column prop="state" label="State" width="120" />
-                    <el-table-column prop="city" label="City" width="120" /> -->
-                    <el-table-column prop="description" label="Description" width="500" />
-                    <el-table-column prop="money" label="Money" width="120" />
-                    <el-table-column fixed="right" label="Operations" width="120">
-                        <template #default="scope">
-                            <el-button link type="primary" size="small" @click.prevent="deleteRow(scope.$index)">
-                                Remove
-                            </el-button>
-                        </template>
-                    </el-table-column>
-                </el-table>
-                <el-button class="mt-4" style="width: 100%" @click="this.dialogFormVisible = true;">Add Item</el-button>
+                <template #header>
+                    <div class="card-header">
+                        <span>预算申请记录</span>
+                        <el-button class="button" color="#023764" @click="dialogFormVisible = true">提交申请
+                        </el-button>
+                    </div>
+                </template>
 
-                <div>Total Amount: {{ apply.amount }}</div>
-                <el-dialog v-model="dialogFormVisible" title="Shipping address">
-                    <el-form :model="item">
-                        <el-form-item label="Name" :label-width="formLabelWidth">
+                <el-dialog width="80%" v-model="dialogFormVisible" title="New Budget Apply">
+                    <el-form>
+                        <el-form-item label="Title ">
+                            <el-input style="width: 180px;" v-model="apply.title" />
+                        </el-form-item>
+                    </el-form>
+
+
+                    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+                        <el-form-item label="Name">
                             <el-input v-model="item.name" autocomplete="off" />
                         </el-form-item>
-                        <el-form-item label="Description" :label-width="formLabelWidth">
-                            <el-input v-model="item.description" autocomplete="off" />
+                        <el-form-item label="Money">
+                            <el-input type="number" min="0" v-model="item.money" autocomplete="off" />
                         </el-form-item>
-                        <el-form-item label="Money" :label-width="formLabelWidth">
-                            <el-input type="number" v-model="item.money" autocomplete="off" />
+                        <el-form-item label="Description">
+                            <el-input style="width: 300px;" v-model="item.description" autocomplete="off" />
                         </el-form-item>
-
+                        <el-form-item>
+                            <el-button type="primary" @click="addItem">Add</el-button>
+                        </el-form-item>
                     </el-form>
-                    <template #footer>
-                        <span class="dialog-footer">
-                            <el-button @click="dialogFormVisible = false">Cancel</el-button>
-                            <el-button type="primary" @click="dialogFormVisible = false; addItem()">
-                                Confirm
-                            </el-button>
-                        </span>
-                    </template>
+
+                    <el-table :data="tableData" style="width: 100%" max-height="250">
+                        <el-table-column prop="name" label="Name" width="220" />
+                        <el-table-column prop="description" label="Description" width="500" />
+                        <el-table-column prop="money" label="Money" width="120" />
+                        <el-table-column fixed="right" label="Operations" width="120">
+                            <template #default="scope">
+                                <el-button link type="primary" size="small" @click.prevent="deleteRow(scope.$index)">
+                                    Remove
+                                </el-button>
+                            </template>
+                        </el-table-column>
+                    </el-table>
+
+
+                    <div>Total Amount: {{ apply.amount }}</div>
+
+
+                    <el-button type="primary" @click="addBudget">确认</el-button>
                 </el-dialog>
 
 
-                <!-- <template #header>
-                    <div class="card-header">
-                        <span>提交预算</span>
-                    </div>
-                </template> -->
-                <!-- 交预算表 -->
-                <!-- <el-form :model="budget">
-                    <el-form-item label="申请理由" label-width="110px">
-                        <el-input v-model="budget.budgetApplyReason" autocomplete="off" type="textarea" :rows="6" />
-                    </el-form-item>
-                </el-form>
-                 -->
-                <el-button type="primary" @click="addBudget">确认</el-button>
-                
                 <br>
                 <el-tag class="ml-2" type="warning" size="large">Only main function operating. Still in process</el-tag>
             </el-card>
@@ -109,8 +104,15 @@ export default {
     },
     methods: {
         addItem() {
-            this.item.money=parseInt(this.item.money);
-            this.apply.amount=this.apply.amount+this.item.money;
+            if (this.item.name == '' || this.item.value <= 0) {
+                ElMessage({
+                    message: "invalid input!",
+                    type: 'warning',
+                })
+                return;
+            }
+            this.item.money = parseInt(this.item.money);
+            this.apply.amount = this.apply.amount + this.item.money;
             this.tableData.push(this.item);
             this.item = {
                 name: '',
